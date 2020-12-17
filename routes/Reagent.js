beb89813f5c7afb5a12ca3eece152124a2e959d4
Sherlocked21B <bijayalamichhane13@gmail.com>
2020-12-17 23:01:42 +0545

    Routes updated

--- routes/Reagent.js
+++ routes/Reagent.js
@@ -1,65 +1,57 @@
 const express = require('express');
 router = express.Router();
 //Importing Schema
-const Customer = require('../model/Customer');
+const Reagent = require('../model/Reagent');
 
 router.get('/', (req, res) => {
-	Customer.find()
-		.then((customer) => res.json(customer))
+	Reagent.find()
+		.then((reagent) => res.json(reagent))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.post('/add', (req, res) => {
 	const name = req.body.name;
-	const age = Number(req.body.age);
-	const address = req.body.address;
-	const fee = Number(req.body.fee);
-	const gender = req.body.gender;
-	const test = req.body.test;
-	const sample = req.body.sample;
+	const unit = req.body.unit;
+	const volume = Number(req.body.volume);
 
-	const newCustomer = new Customer({
+	const newReagent = new Reagent({
 		name,
-		age,
-		address,
-		fee,
-		gender,
-		test,
-		sample,
+        unit,
+        volume
 	});
 
-	newCustomer
+	newReagent
 		.save()
-		.then(() => res.json('Customer Added'))
+		.then(() => res.json('Reagent Added'))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.get('/:id', (req, res) => {
-	Customer.findById(req.params.id)
-		.then((customer) => res.json(customer))
+	Reagent.findById(req.params.id)
+		.then((reagent) => res.json(reagent))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.put('/update/:id', (req, res) => {
 	const fields = Object.keys(req.body);
 
-	Customer.findByIdAndUpdate(req.params.id)
-		.then((customer) => {
+	Reagent.findByIdAndUpdate(req.params.id)
+		.then((reagent) => {
 			fields.forEach((field) => {
-				customer[field] = req.body[field];
+				reagent[field] = req.body[field];
 			});
 
-			customer
+			reagent
 				.save()
-				.then(() => res.json('Customer Updated'))
+				.then(() => res.json('Reagent Updated'))
 				.catch((err) => res.status(400).json('Error:' + err));
 		})
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.delete('/delete/:id', (req, res) => {
-	Customer.findByIdAndDelete(req.params.id)
-		.then((customer) => res.json(customer))
+	Reagent.findByIdAndDelete(req.params.id)
+		.then((reagent) => res.json(reagent))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
--- routes/Test.js
+++ routes/Test.js
@@ -1,65 +1,58 @@
 const express = require('express');
 router = express.Router();
 //Importing Schema
-const Customer = require('../model/Customer');
+const Test = require('../model/Test');
+const Parameter = require('../model/Parameter');
 
 router.get('/', (req, res) => {
-	Customer.find()
-		.then((customer) => res.json(customer))
+	Test.find()
+		.then((test) => res.json(test))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.post('/add', (req, res) => {
 	const name = req.body.name;
-	const age = Number(req.body.age);
-	const address = req.body.address;
-	const fee = Number(req.body.fee);
-	const gender = req.body.gender;
-	const test = req.body.test;
-	const sample = req.body.sample;
+	const amount = Number(req.body.unit);
+	const parameter = Parameter(req.body.volume);
 
-	const newCustomer = new Customer({
+	const newTest = new Test({
 		name,
-		age,
-		address,
-		fee,
-		gender,
-		test,
-		sample,
+        amount,
+        parameter
 	});
 
-	newCustomer
+	newTest
 		.save()
-		.then(() => res.json('Customer Added'))
+		.then(() => res.json('Test Added'))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.get('/:id', (req, res) => {
-	Customer.findById(req.params.id)
-		.then((customer) => res.json(customer))
+	Test.findById(req.params.id)
+		.then((test) => res.json(test))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.put('/update/:id', (req, res) => {
 	const fields = Object.keys(req.body);
 
-	Customer.findByIdAndUpdate(req.params.id)
-		.then((customer) => {
+	Test.findByIdAndUpdate(req.params.id)
+		.then((test) => {
 			fields.forEach((field) => {
-				customer[field] = req.body[field];
+				test[field] = req.body[field];
 			});
 
-			customer
+			test
 				.save()
-				.then(() => res.json('Customer Updated'))
+				.then(() => res.json('Test Updated'))
 				.catch((err) => res.status(400).json('Error:' + err));
 		})
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
 router.delete('/delete/:id', (req, res) => {
-	Customer.findByIdAndDelete(req.params.id)
-		.then((customer) => res.json(customer))
+	Test.findByIdAndDelete(req.params.id)
+		.then((test) => res.json(test))
 		.catch((err) => res.status(400).json('Error:' + err));
 });
 
