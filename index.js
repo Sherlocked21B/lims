42472a601f32cd4209bacb5017c39a3d0e27ab3e
unknown <anishniraula85@gmail.com>
2021-03-12 20:41:08 +0545

    customer and sample fixed

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const donenv = require('dotenv');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const donenv = require("dotenv");
 donenv.config();
 
 //importing the routes
-const authRoute = require('./routes/auth');
-const customerRoute = require('./routes/Customer');
-const reagentRoute = require('./routes/Reagent');
-const sampleRoute = require('./routes/Sample');
-const testRoute = require('./routes/Test');
+const authRoute = require("./routes/auth");
+const customerRoute = require("./routes/Customer");
+const reagentRoute = require("./routes/Reagent");
+const sampleRoute = require("./routes/Sample");
+const testRoute = require("./routes/Test");
 
 //milldleware imports
-const isStaff = require('./middlewares/isStaff');
-const isInventoryManager = require('./middlewares/isInventoryManager');
-const isStafforAccountant = require('./middlewares/isStafforAccountant');
+const isStaff = require("./middlewares/isStaff");
+const isInventoryManager = require("./middlewares/isInventoryManager");
+const isStafforAccountant = require("./middlewares/isStafforAccountant");
 
 app.use(cors());
 app.use(express.json());
 
 //connecting to the mongo db
 mongoose.connect(
-	process.env.db_name,
-	{
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	},
-	() => console.log('we are connected to the database')
+  process.env.db_name,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => console.log("we are connected to the database")
 );
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log('MongoDB Connection established SUcessfully');
+connection.once("open", () => {
+  console.log("MongoDB Connection established SUcessfully");
 });
 
 //backend routes
-app.use('/', authRoute);
-app.use('/customer', isStafforAccountant, customerRoute);
-app.use('/reagent', isInventoryManager, reagentRoute);
-app.use('/sample', isStaff, sampleRoute);
-app.use('/test', isStaff, testRoute);
+app.use("/", authRoute);
+app.use("/customer", isStafforAccountant, customerRoute);
+app.use("/reagent", isInventoryManager, reagentRoute);
+app.use("/sample", isStaff, sampleRoute);
+app.use("/test", isStaff, testRoute);
 
-app.listen(3000, () => console.log('server has started'));
+app.listen(5000, () => console.log("server has started"));
--- routes/Customer.js
+++ routes/Customer.js
@@ -1,89 +1,84 @@
-const express = require('express');
+const express = require("express");
 router = express.Router();
 //Importing Schema
-const Customer = require('../model/Customer');
+const Customer = require("../model/Customer");
 
-router.get('/', (req, res) => {
-	Customer.find()
-		.then((customer) => res.json(customer))
-		.catch((err) => res.status(400).json('Error:' + err));
+router.get("/", (req, res) => {
+  Customer.find()
+    .then((customer) => res.json(customer))
+    .catch((err) => res.status(400).json("Error:" + err));
 });
 
-router.post('/add', (req, res) => {
-	// const firstName = req.body.firstName;
-	// const lastName = req.body.lastName;
-	// const age = Number(req.body.age);
-	// const address = req.body.address;
-	// const fee = Number(req.body.fee);
-	// const gender = req.body.gender;
-	// const test = req.body.test;
-	// const sample = req.body.sample;
+router.post("/add", (req, res) => {
+  // const firstName = req.body.firstName;
+  // const lastName = req.body.lastName;
+  // const age = Number(req.body.age);
+  // const address = req.body.address;
+  // const fee = Number(req.body.fee);
+  // const gender = req.body.gender;
+  // const test = req.body.test;
+  // const sample = req.body.sample;
 
-	//auto conversion from string to number by mongoose
+  //auto conversion from string to number by mongoose
 
-	const {
-		firstName,
-		lastName,
-		age,
-		address,
-		fee,
-		gender,
-		test,
-		sample,
-	} = req.body;
+  const { firstName, lastName, age, address, gender } = req.body;
 
-	const newCustomer = new Customer({
-		firstName,
-		lastName,
-		age,
-		address,
-		fee,
-		gender,
-		test,
-		sample,
-	});
+  const newCustomer = new Customer({
+    firstName,
+    lastName,
+    age,
+    address,
+    gender,
+  });
 
-	newCustomer
-		.save()
-		.then(() => res.json('Customer Added'))
-		.catch((err) => res.status(400).json('Error:' + err));
+  newCustomer
+    .save()
+    .then(() => res.json("Customer Added"))
+    .catch((err) => res.status(400).json("Error:" + err));
 });
 
-router.get('/:id', (req, res) => {
-	Customer.findById(req.params.id)
-		.then((customer) => res.json(customer))
-		.catch((err) => res.status(400).json('Error:' + err));
+router.get("/:id", (req, res) => {
+  Customer.findById(req.params.id)
+    .then((customer) => res.json(customer))
+    .catch((err) => res.status(400).json("Error:" + err));
 });
 
-router.put('/update/:id', (req, res) => {
-	// const fields = Object.keys(req.body);
+router.put("/update/:id", (req, res) => {
+  // const fields = Object.keys(req.body);
 
-	// Customer.findByIdAndUpdate(req.params.id)
-	// 	.then((customer) => {
-	// 		fields.forEach((field) => {
-	// 			customer[field] = req.body[field];
-	// 		});
+  // Customer.findByIdAndUpdate(req.params.id)
+  // 	.then((customer) => {
+  // 		fields.forEach((field) => {
+  // 			customer[field] = req.body[field];
+  // 		});
 
-	// 		customer
-	// 			.save()
-	// 			.then(() => res.json('Customer Updated'))
-	// 			.catch((err) => res.status(400).json('Error:' + err));
-	// 	})
-	// 	.catch((err) => res.status(400).json('Error:' + err));
+  // 		customer
+  // 			.save()
+  // 			.then(() => res.json('Customer Updated'))
+  // 			.catch((err) => res.status(400).json('Error:' + err));
+  // 	})
+  // 	.catch((err) => res.status(400).json('Error:' + err));
 
-	//Refractor in easy method
-	Customer.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
-		if (err) {
-			return res.status(400).json({ message: err });
-		}
-		res.json(doc);
-	});
+  //Refractor in easy method
+  Customer.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
+    if (err) {
+      return res.status(400).json({ message: err });
+    }
+    res.json(doc);
+  });
 });
 
-router.delete('/delete/:id', (req, res) => {
-	Customer.findByIdAndDelete(req.params.id)
-		.then((customer) => res.json(customer))
-		.catch((err) => res.status(400).json('Error:' + err));
+router.delete("/delete/:id", (req, res) => {
+  Customer.findByIdAndDelete(req.params.id)
+    .then((customer) => res.json(customer))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+router.get("/search/:query", (req, res) => {
+  const term = RegExp(`${req.params.query}`, "i");
+  Customer.find({ firstName: term })
+    .then((customers) => res.json(customers))
+    .catch((err) => res.status(400).json("Error:" + err));
 });
 
 module.exports = router;
