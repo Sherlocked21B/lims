68844d458527e2bb06b80d69fe4543ebd7c2f46c
unknown <anishniraula85@gmail.com>
2021-03-29 20:33:55 +0545

    test and user route modify

--- model/Test.js
+++ model/Test.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
-const parameter = require("./Parameter")
+const mongoosePaginate = require("mongoose-paginate-v2");
+const parameter = require("./Parameter");
 
 const testSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-   amount:{
-       type:Number,
-       required:true
-   },
-   parameter:[parameter]
+  name: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  parameter: [parameter],
 });
-
-module.exports = mongoose.model("Test",testSchema);
+testSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model("Test", testSchema);
--- utils/validation.js
+++ utils/validation.js
@@ -1,23 +1,20 @@
 const joi = require("@hapi/joi");
 
 const registervalidation = (data) => {
-    const schema = joi.object({
-        userName: joi.string().min(4).required(),
-        password: joi.string().min(6).required(),
-        role: joi.string().required(),
-        adminId:joi.string().required(),
-        adminPassword:joi.string().min(6).required()
-
-    });
-    return schema.validate(data);
+  const schema = joi.object({
+    userName: joi.string().min(4).required(),
+    password: joi.string().min(6).required(),
+    role: joi.string().required(),
+  });
+  return schema.validate(data);
 };
 
 const loginvalidation = (data) => {
-    const schema = joi.object({
-        userName: joi.string().min(4).required(),
-        password: joi.string().min(6).required(),
-    });
-    return schema.validate(data);
+  const schema = joi.object({
+    userName: joi.string().min(4).required(),
+    password: joi.string().min(6).required(),
+  });
+  return schema.validate(data);
 };
 
 module.exports.registervalidation = registervalidation;
