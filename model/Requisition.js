435c3060509df69af2bf6f72959941896de2369a
Sherlocked21B <bijayalamichhane13@gmail.com>
2021-07-19 19:44:07 +0545

    requsition search added

--- model/Requisition.js
+++ model/Requisition.js
@@ -1,20 +1,24 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate-v2');
-
-const requisitionSchema = new mongoose.Schema({
-	request: [
-		{
-			name: String,
-			unit: String,
-			quantity: Number,
+const requisitionSchema = new mongoose.Schema(
+	{
+		request: [
+			{
+				name: String,
+				unit: String,
+				quantity: Number,
+			},
+		],
+		created_at: {
+			type: Date,
+			default: new Date(
+				new Date().toISOString().substring(0, 10),
+			).toISOString(),
 		},
-	],
-	createdAt: {
-		type: Date,
-		default: new Date(new Date().toISOString().substring(0, 10)).toISOString(),
 	},
-});
+	{ timestamps: true },
+);
 requisitionSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model("Requisition", requisitionSchema);
--- routes/Requisition.js
+++ routes/Requisition.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 router = express.Router();
 
-const Requisition = require('../model/Requisition');
+const Requisition = require("../model/Requisition");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
 	let page = req.query.page;
 	let limit = req.query.limit;
 
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
 	});
 });
 
-router.post('/add', (req, res) => {
+router.post("/add", (req, res) => {
 	const { request } = req.body;
 
 	const newResult = new Requisition({
@@ -30,16 +30,16 @@ router.post('/add', (req, res) => {
 	newResult
 		.save()
 		.then((result) => res.json(result))
-		.catch((err) => res.status(400).json('Error:' + err));
+		.catch((err) => res.status(400).json("Error:" + err));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
 	Requisition.findById(req.params.id)
 		.then((result) => res.json(result))
-		.catch((err) => res.status(400).json('Error:' + err));
+		.catch((err) => res.status(400).json("Error:" + err));
 });
 
-router.put('/update/:id', (req, res) => {
+router.put("/update/:id", (req, res) => {
 	Requisition.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
 		if (err) {
 			return res.status(400).json({ message: err });
@@ -48,10 +48,29 @@ router.put('/update/:id', (req, res) => {
 	});
 });
 
-router.delete('/delete/:id', (req, res) => {
+router.get("/search/date", (req, res) => {
+	let date = req.query.date;
+	let page = req.query.page;
+	let limit = req.query.limit;
+
+	const options = {
+		offset: page ? page * limit : 0,
+		limit: limit ? limit : 20,
+		sort: { createdAt: -1 },
+	};
+
+	Requisition.paginate({ created_at: date }, options, (err, result) => {
+		if (err) {
+			return res.status(400).json({ message: err });
+		}
+		res.json({ rows: result.docs, total: result.totalDocs });
+	});
+});
+
+router.delete("/delete/:id", (req, res) => {
 	Requisition.findByIdAndDelete(req.params.id)
 		.then((result) => res.json(result))
-		.catch((err) => res.status(400).json('Error:' + err));
+		.catch((err) => res.status(400).json("Error:" + err));
 });
 
 module.exports = router;
